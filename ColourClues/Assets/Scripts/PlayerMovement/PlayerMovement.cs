b7753da2c6ffbb5432ef04ac12ae6dc7192ca7d4
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2022-07-08 20:25:59 +0200

    Level 2 add Ladder Movement

--- ColourClues/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ ColourClues/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5.0f;
     [SerializeField] private float jumpPower = 5.0f;
+
+    public GameObject[] ladders;
+    private string[] ladderNames; 
     
     private Rigidbody2D _playerRigidbody;
+
+    private bool moveUp = false;
+
     private void Start()
     {
         _playerRigidbody = GetComponent<Rigidbody2D>();
@@ -15,6 +22,14 @@ public class PlayerMovement : MonoBehaviour
         {
             Debug.LogError("Player is missing a Rigidbody2D component");
         }
+
+        ladderNames = new string[ladders.Length];
+
+        for (int i = 0; i < ladderNames.Length; ++i) 
+        {
+            ladderNames[i] = ladders[i].name;
+        }
+
     }
     private void Update()
     {
@@ -25,6 +40,11 @@ public class PlayerMovement : MonoBehaviour
     }
     private void MovePlayer()
     {
+        if(moveUp){
+            var verticalInput = Input.GetAxisRaw("Vertical");
+            _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x,verticalInput * playerSpeed);
+        }
+
         var horizontalInput = Input.GetAxisRaw("Horizontal");
         _playerRigidbody.velocity = new Vector2(horizontalInput * playerSpeed, _playerRigidbody.velocity.y);
     }
@@ -34,6 +54,26 @@ public class PlayerMovement : MonoBehaviour
     {
         var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.7f);
         return groundCheck.collider != null && groundCheck.collider.CompareTag("Ground");
+    }
+
+    private void OnTriggerEnter2D(Collider2D col){
+
+        Debug.Log("Trigger Entered");
+        if(ladderNames.Contains(col.name)){
+            moveUp = true;
+        }
+        
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D col){
+
+        Debug.Log("Trigger Exited");
+        if(ladderNames.Contains(col.name)){
+            moveUp = false;
+        }
+        
+        
     }
 
    
