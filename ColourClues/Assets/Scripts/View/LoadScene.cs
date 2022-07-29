05444aea67ddfb0686cd1d9101f09eeec2428bf8
kev <57128890+Ktechen@users.noreply.github.com>
2022-07-29 17:32:53 +0200

    LoadScene comments

--- ColourClues/Assets/Scripts/View/LoadScene.cs
+++ ColourClues/Assets/Scripts/View/LoadScene.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Load scene via index
+/// </summary>
 public class LoadScene : MonoBehaviour
 {
     public void loadScene(int index)
